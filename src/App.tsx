580b38d96211e538d37125f7ea953a1c34e72680
Nastia Karpova <akkarpova@gmail.com>
2023-07-18 13:50:24 +0200

    Style initial input field and submit button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 // import { useState } from 'react'
 import './App.css'
 
-export default function App() {
+function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
       <h1>Todo App</h1>
       {/* <label htmlFor="task-input">Here</label> */}
-      <input type="text" className="task-input" placeholder='I have to...'/>
-      <button className='add-btn'>+</button>
+      <div className='task-div'>
+        <input 
+          type="text" 
+          className="task-input" 
+          placeholder='Write your task'
+        />
+        <button 
+          className='add-btn'>
+            +
+        </button>
+      </div>
     </>
     
   )
 }
+
+export default App;
