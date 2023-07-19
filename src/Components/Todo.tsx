da1195f8ea36a3ea32d90cef7ef8fbc363966fc9
Nastia Karpova <akkarpova@gmail.com>
2023-07-19 12:53:38 +0200

    Read data (tasks) from a file data.js

--- src/Components/Todo.tsx
+++ src/Components/Todo.tsx
@@ -4,7 +4,15 @@ import React from "react"
 function Todo(props) {
     return(
         <div>
-            test
+            <div>
+                
+                    <input type="checkbox" id={props.item.id} 
+                        name="task-inp" className="task-li" />
+                    <label 
+                        for="task-inp">{props.item.description}
+                    </label>
+                
+            </div>
 
         </div>
     );
